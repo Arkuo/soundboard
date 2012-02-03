1987924ad020e8144799b85da27c9649623bfde3
Jan Krutisch <jan@krutisch.de>
2012-02-03 17:04:20 +0100

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundboard::Application.config.secret_token = '433bb66b77622653d833f995b173395e36bf036bc83efb05cbe6fc67e5327ebc839d7cb999abf84bd325ace06dae2ac359a7f421496ff2732078550d1bc407d8'
