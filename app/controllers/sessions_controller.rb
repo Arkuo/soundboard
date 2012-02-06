fa1d850dd3dd28017a7061fdeb7565b2c4902e8a
Jan Krutisch <jan@krutisch.de>
2012-02-06 09:54:55 +0100

    minimal design, trying to restrict to a certain domain

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
   def create
     self.current_user = User.find_or_create_from_auth_hash(auth_hash)
+    if logged_in?
+      flash[:notice] = "You're logged in"
+    else
+      flash[:notice] = "You're not allowed here"
+    end
     redirect_to root_path
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   has_many :soundboard_sounds, :through => :soundboard_inclusions, :source => :sounds
 
   def self.find_or_create_from_auth_hash(hash)
+    if email_restriction = ENV['APPS_RESTRICTION']
+      return nil unless hash['info']['email'].match(/#{email_restriction}$/)
+    end
     user = User.find_or_initialize_by_uid(hash['uid'], {email: hash['info']['email']})
     user.token = hash['credentials']['token']
     user.save
